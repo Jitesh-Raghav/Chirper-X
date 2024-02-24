a09bea6c8038bb2e4e809ad0d1856b921f8e1be6
Jitesh Kumar Raghav <jkr18042001@gmail.com>
2024-02-24 23:51:05 +0530

    added tweet section

--- src/Components/HomeSection/HomeSection.jsx
+++ src/Components/HomeSection/HomeSection.jsx
@@ -1,13 +1,19 @@
-import { Avatar } from '@mui/material'
+import { Avatar, Button } from '@mui/material'
 import { useFormik } from 'formik'
-import React from 'react'
+import React, { useState } from 'react'
 import * as Yup from 'yup'
+import ImageIcon from '@mui/icons-material/Image';
+import FmdGoodIcon from '@mui/icons-material/FmdGood';
+import TagFacesIcon from '@mui/icons-material/TagFaces';
 
-const validationSchema= Yup.object().shape({
+const validationSchema= Yup.object().shape({     //for validation purposes
     content:Yup.string().required("Tweet text is required")
 })
 
 const HomeSection = () => {
+
+  const [uploadingImage, setUploadingImage]=useState(false);
+  const [selectedImage, setSelectedImage]= useState();
 
     const handleSubmit=(values)=>{
      console.log("values ", values)
@@ -21,6 +27,15 @@ const HomeSection = () => {
     onSubmit:handleSubmit, 
     validationSchema
    });
+
+   const handleSelectImage=(event)=>{
+     setUploadingImage(true);
+     const imgUrl= event.target.files[0]
+     formik.setFieldValue("image", imgUrl);
+     setSelectedImage(imgUrl);
+     setUploadingImage(false);
+      
+   };
 
   return (
     <div className="space-y-5">
@@ -41,6 +56,22 @@ const HomeSection = () => {
                         <span className='text-red-500'>{formik.errors.content}</span>
                       )}
                     </div>
+
+                    <div className="flex justify-between items-center mt-5">
+                      <div className="flex items-center space-x-5">
+                        <label className="flex items-center cursor-pointer rounded-sm space-x-2">
+                           <ImageIcon className="text-[#2196f3]"/>
+                           <input type="file" name="imageFile" className="hidden" onChange={handleSelectImage}/>
+                        </label>
+                         <FmdGoodIcon className="text-[#2196f3]"/>
+                         <TagFacesIcon className="text-[#2196f3]"/>
+                      </div>
+
+                      <div className='flex justify'>
+                        <Button sx={{ width: "100%", borderRadius: "20px", paddingY: "8px", paddingX:"20px", bgcolor: "#2196f3" }} variant='contained'>Tweet</Button>
+                      </div>
+                    </div>
+
                 </form>
             </div>
         </div>
