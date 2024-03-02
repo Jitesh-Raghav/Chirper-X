6af651eb3254b56ec7d4044057cb46c8de003844
Jitesh Kumar Raghav <jkr18042001@gmail.com>
2024-03-02 15:09:34 +0530

    added the reply modal

--- src/Components/HomeSection/ReplyModal.jsx
+++ src/Components/HomeSection/ReplyModal.jsx
@@ -1,41 +1,136 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Modal from '@mui/material/Modal';
+import { useNavigate } from 'react-router-dom';
+import VerifiedIcon from '@mui/icons-material/Verified';
+import { Avatar } from '@mui/material';
+import ImageIcon from '@mui/icons-material/Image';
+import FmdGoodIcon from '@mui/icons-material/FmdGood';
+import TagFacesIcon from '@mui/icons-material/TagFaces';
+import { useFormik } from 'formik';
 
 
 const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 600,
-    bgcolor: 'background.paper',
-    border: 'none',
-    outline:'none',
-    boxShadow: 24,
-    p: 4,
-    borderRadius:4
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 600,
+  bgcolor: 'background.paper',
+  border: 'none',
+  outline: 'none',
+  boxShadow: 24,
+  p: 4,
+  borderRadius: 4
+};
+
+export default function ReplyModal() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const navigate = useNavigate();
+
+  const [uploadingImage, setUploadingImage] = useState(false);
+  const [selectedImage, setSelectedImage] = useState();
+
+  const handleSubmit = (values) => {
+    console.log("handle submit", values)
+  }
+
+  const formik = useFormik({
+    initialValues: {
+      content: "",
+      image: "",
+      tweetId: 4,
+    },
+    onSubmit: handleSubmit
+  })
+
+
+
+  const handleSelectImage = (event) => {
+    setUploadingImage(true);
+    const imgUrl = event.target.files[0]
+    formik.setFieldValue("image", imgUrl);
+    setSelectedImage(imgUrl);
+    setUploadingImage(false);
+
   };
 
-    export default function ReplyModal() {
-      const [open, setOpen] = React.useState(false);
-      const handleOpen = () => setOpen(true);
-      const handleClose = () => setOpen(false);
-    
-      return (
-        <div>
-          <Modal
-            open={true}
-            onClose={handleClose}
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description"
-          >
-            <Box sx={style}>
-              
-            </Box>
-          </Modal>
-        </div>
-      );
-    }
+  return (
+    <div>
+      <Modal
+        open={true}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <div className='flex space-x-5'>
+            <Avatar onClick={() => navigate("/profile/${5}")} className="cursor-pointer" alt="username" src="https://avatars.githubusercontent.com/u/93904444?v=4" />
+
+            <div className="w-full">
+              <div className="flex justify-between items-center">
+                <div className="flex items-center cursor-pointer space-x-2">
+
+                  <span className="font-semibold">Jitesh Raghav</span>
+                  <VerifiedIcon fontSize='small' className="text-[#2196f3]" />
+                  <span className="text-gray-600">@okayJitesh .    2m</span>
+
+                </div>
+
+              </div>
+
+              <div className="mt-2">
+                <div className="cursor-pointer w-full" onClick={() => navigate("/tweet/${3}")}>
+                  <p className='mb-2 p-0 flex items-start'>Life's gonna hit you hard, hit back harder!!👊<span className="text-[#2196f3]">#AOT #Anime</span></p>
+                  <img className="border border-gray-400 p-2  rounded-sm w-[34rem]" src="https://wallpapers-clan.com/wp-content/uploads/2023/10/aot-fury-titan-desktop-wallpaper-preview.jpg" alt="" />
+                </div>
+              </div>
+
+            </div>
+          </div>
+
+
+
+          <section className="py-5 mt-4 flex items-start">
+            <div className="flex space-x-5">
+              <Avatar alt="username" src="https://avatars.githubusercontent.com/u/93904444?v=4" />
+              <div className="w-full">
+                <form onSubmit={formik.handleSubmit}>
+                  <div>
+                    <input type="text" name="content" placeholder='What is happening?'
+                      className="border-none outline-none text-xl bg-transparent flex items-start"
+                      {...formik.getFieldProps("content")} />
+                    {formik.errors.content && formik.touched.content && (
+                      <span className='text-red-500 flex items-start'>{formik.errors.content}</span>
+                    )}
+                  </div>
+
+                  <div className="flex justify-between items-center mt-5">
+                    <div className="flex items-center space-x-5">
+                      <label className="flex items-center cursor-pointer rounded-sm space-x-2">
+                        <ImageIcon className="text-[#2196f3]" />
+                        <input type="file" name="imageFile" className="hidden" onChange={handleSelectImage} />
+                      </label>
+                      <FmdGoodIcon className="text-[#2196f3] cursor-pointer" />
+                      <TagFacesIcon className="text-[#2196f3] cursor-pointer" />
+                    </div>
+
+                    <div className="ml-[270px]">
+                      <Button sx={{ width: "100%", borderRadius: "20px", py: "8px", px: "20px", bgcolor: "#2196f3" }} variant='contained' type="submit">Tweet</Button>
+                    </div>
+                  </div>
+
+                </form>
+              </div>
+            </div>
+          </section>
+
+        </Box>
+      </Modal>
+    </div>
+  );
+}
 
