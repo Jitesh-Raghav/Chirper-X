d8e9dc54eedf4205ee7e2187c0fffff8c75f3511
Jitesh Kumar Raghav <jkr18042001@gmail.com>
2024-04-16 13:52:15 +0530

    completed authentication page

--- src/Components/Authentication/Authentication.jsx
+++ src/Components/Authentication/Authentication.jsx
@@ -4,7 +4,7 @@ import { GoogleLogin } from '@react-oauth/google';
 
 const Authentication = () => {
   return (
-    <div>
+    <div className='bg-black text-white'>
        <Grid className="overflow-y-hidden" container>
           <Grid className="hidden lg:block" item lg={7}>
 
@@ -19,14 +19,20 @@ const Authentication = () => {
           </Grid>
 
           <Grid className='px-10' item lg={5} xs={12}>
-            <h1 className='font-bold text-7xl'>Happening Now</h1>
-            <h1 className='font-bold text-3xl py-16'>Join Twitter Today</h1>
+            <h1 className='font-bold text-7xl mt-16'>Happening Now</h1>
+            <h1 className='font-bold text-3xl pt-16 pb-10 flex items-start'>Join today.</h1>
           
             <div className='w-[60%]'>
                <div className='w-full'>
                  <GoogleLogin width={330}/>
                  <p className='py-5 text-center'>OR</p>
-                 <Button fullWidth variant="contained" size="large" sx={{borderRadius:"29px", py:"7px"}}>Create Account</Button>
+                 <Button fullWidth variant="contained" size="large" sx={{borderRadius:"29px", py:"7px", textTransform:"none"}}>Create account</Button>
+                 <p className='text-xs mt-1 text-justify text-gray-500 text leading-tight'>By signing up, you agree to the <span className='text-blue-400 hover:underline cursor-pointer'>Terms of Service</span> and <span className='text-blue-400 hover:underline cursor-pointer'>Privacy Policy</span>, including <span className='text-blue-400 hover:underline cursor-pointer'>Cookie Use</span>.</p>
+               </div>
+
+               <div>
+                <h1 className='mt-10 mb-4 flex items-start'>Already Have Account?</h1>
+                <Button fullWidth variant="outlined" size="large" sx={{borderRadius:"29px", py:"7px", textTransform:"none"}}>Sign In</Button>
                </div>
             </div>
           
--- src/index.js
+++ src/index.js
@@ -6,13 +6,16 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { store } from './Store/store';
 import { Provider } from 'react-redux';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
     <Provider store={store}>
-      <App/>
+      <GoogleOAuthProvider>
+         <App/>
+      </GoogleOAuthProvider>
     </Provider>
     </BrowserRouter>
   </React.StrictMode>
