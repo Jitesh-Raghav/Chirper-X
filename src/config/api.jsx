5279c3705b98ba14b559801f2b9fddcff66408f6
Jitesh Kumar Raghav <jkr18042001@gmail.com>
2024-04-21 10:17:07 +0530

    made actions for Tweet

--- src/config/api.jsx
+++ src/config/api.jsx
@@ -1,1 +1,9 @@
 export const API_BASE_URL="http://localhost:8080"
+
+export const api= axios.create({
+    baseUrl:API_BASE_URL,
+    headers:{
+        "Authorization":`Bearer ${localStorage.getItem("jwt")}`,
+        "Content-Type":"application/json"
+    }
+})
