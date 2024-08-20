6093e5c85b0f60164dacf1da67e0a12face8a29e
Jitesh Kumar Raghav <jkr18042001@gmail.com>
2024-08-21 01:12:53 +0530

    using cloudflared tunneling

--- src/config/api.jsx
+++ src/config/api.jsx
@@ -1,7 +1,9 @@
 import axios from "axios"
 
 // export const API_BASE_URL="http://localhost:8080"
-export const API_BASE_URL="https://chirper-x-backend-production.up.railway.app"
+//export const API_BASE_URL="https://chirper-x-backend-production.up.railway.app"
+export const API_BASE_URL="https://reserve-contests-yn-beam.trycloudflare.com"
+
 
 export const api= axios.create({
     baseURL:API_BASE_URL,
